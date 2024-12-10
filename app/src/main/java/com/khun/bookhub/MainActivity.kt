839efa79cbc7a8ece7b7d7fe3ec22dcb238d3e2b
KHUN SOE MOE AUNG <prince.sma07@gmail.com>
2024-12-10 16:28:37 -0600

    Initialize Project

---
+++ app/src/main/java/com/khun/bookhub/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.khun.bookhub
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.khun.bookhub.ui.theme.BookHubTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BookHubTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+//Reference
+//https://github.com/Vibhav00/BookApnahAndroidApp?tab=readme-ov-file
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BookHubTheme {
+        Greeting("Android")
+    }
+}
